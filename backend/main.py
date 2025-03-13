73b483ba2c9483c048593d74eff37d99fa7240fd
Lakshay Jain <lakshaybusiness.12@gmail.com>
2025-03-13 20:19:56 +0530

    Added documentation and comments

--- backend/main.py
+++ backend/main.py
@@ -1,43 +1,33 @@
-# from modules.scrapper import extractVideoUrls
-
-# """
-# Documentation 
-
-# Function
-# extractVideoUrls -> return a list of video url's eg ['https://www.tiktok.com/@pich.solikah/video/7481208166252891412']
-#     has two parmeters
-#     first parameter
-#     searchQueries -> it should be a list of string eg ["Trump"]
-#     second parameter
-#     hashtags -> it should be a list of string eg ["fyp"]
-
-
-# """
-
-
-
-# def main():
-#     # phase 1 to extract the url's
-#     videoUrls =  extractVideoUrls() 
-#     print(videoUrls) # it will return video url's
-
-#     # phase 2
-
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
 from modules.scrapper import extractVideoUrls
 from modules.downloader import download_videos
 
 
+
+"""
+Documentation 
+
+Function
+extractVideoUrls -> return a list of video url's eg ['https://www.tiktok.com/@pich.solikah/video/7481208166252891412']
+    has two parmeters
+    first parameter
+    searchQueries -> it should be a list of string eg ["Trump"]
+    second parameter
+    hashtags -> it should be a list of string eg ["fyp"]
+
+    
+in second phase
+run this command winget install ffmpeg
+
+
+
+"""
+
 def main():
+    # 1st phase
     print("Fetching TikTok video URLs...")
     video_urls = extractVideoUrls()
     
+    # 2nd phase
     if video_urls:
         print(f"Extracted {len(video_urls)} videos.")
         
@@ -45,6 +35,9 @@ def main():
         download_videos(video_urls)
     else:
         print("No videos found.")
+    
+    # 3rd phase
+    
 
 if __name__ == "__main__":
     main()
