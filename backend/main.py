0f22d05f705dc45cb7bff985fbe0b7cbc7649f07
Lakshay Jain <lakshaybusiness.12@gmail.com>
2025-04-11 10:33:59 +0530

    Fixed console bug

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from modules.downloader import batch_download
 from modules.merge import merging ,count_videos_in_downloads
 from modules.query_hashtag_refiner import refine_query_and_hashtags
 from rich.console import Console
-
+console = Console()  
 """
 Documentation 
 
@@ -22,27 +22,30 @@ extractVideoUrls -> return a list of video url's eg ['https://www.tiktok.com/@pi
 """
 
 def main(searchQueries, hashtags, max_results):
-    use_gemini = input("\nDo you want to use Gemini AI for refining the query and hashtags? (y/n): ").strip().lower() 
-    # 0th phase Refining Queries
-    if use_gemini == "y":
-        searchQueries,hashtags = refine_query_and_hashtags(searchQueries,hashtags)
-        print("Refined Queries",searchQueries)
-        print("Refined Hashtags",hashtags)
+    # use_gemini = input("\nDo you want to use Gemini AI for refining the query and hashtags? (y/n): ").strip().lower() 
+    # # 0th phase Refining Queries
+    # if use_gemini == "y":
+    #     searchQueries,hashtags = refine_query_and_hashtags(searchQueries,hashtags)
+    #     print("Refined Queries",searchQueries)
+    #     print("Refined Hashtags",hashtags)
 
-    # 1st phase (To fetch tik tok URL's)
-    print("Fetching TikTok video URLs...")
-    video_urls = extractVideoUrls(searchQueries, hashtags, max_results)
+    # # 1st phase (To fetch tik tok URL's)
+    # print("Fetching TikTok video URLs...")
+    # video_urls = extractVideoUrls(searchQueries, hashtags, max_results)
             
-    if len(video_urls) == 0:
-        print("No TikTok URLs found. Exiting the program. Please try running the code again.")
-        exit()
+    # if len(video_urls) == 0:
+    #     print("No TikTok URLs found. Exiting the program. Please try running the code again.")
+    #     exit()
+    
+    video_urls=["https://www.tiktok.com/@amusingmichele/video/7491477001946025221",
+        "https://www.tiktok.com/@voteinorout/video/7489981612159946014"]
 
     # 2nd phase (To download videos using tik tok url's)
     if video_urls:
         print(f"Extracted {len(video_urls)} videos.")
         batch_download(video_urls)
         
-        Console.print(f"Manual test complete: {count_videos_in_downloads()} out of {len(video_urls)} videos downloaded successfully.", style="bold yellow")
+        console.print(f"Manual test complete: {count_videos_in_downloads()} out of {len(video_urls)} videos downloaded successfully.", style="bold yellow")
 
         # Phase 3: Ask user if they want to merge the downloaded videos
         user_input = input("Do you want to merge the videos currently in the Downloads folder? Type 'y' for yes or 'n' for no: ")
